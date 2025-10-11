41558e681e046f4f930d0dd0f4894f4049d8b6a0
Daniel Dao <danieldkdao@gmail.com>
2025-10-11 14:39:04 -0500

    header complete

---
+++ components/Navbar.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "@/public/math-app-logo.png";
+import Link from "next/link";
+import { FaArrowLeft, FaBars, FaRightToBracket } from "react-icons/fa6";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div className="w-full flex justify-between items-center sticky top-0 p-2 pr-4">
+      <div className="flex items-center gap-18">
+        <Image src={Logo} alt="Math app logo image" height={90} />
+        <div className="items-center justify-center gap-6 hidden md:flex">
+          <Link href="/" className="text-xl font-medium">
+            Home
+          </Link>
+          <Link href="/about-us" className="text-xl font-medium">
+            About us
+          </Link>
+          <Link href="get-started" className="text-xl font-medium">
+            Get started
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-6 cursor-pointer">
+        <button
+          onClick={() => setShowMenu(true)}
+          className="cursor-pointer active:scale-95 transition-transform duration-100 ease-in-out md:hidden"
+        >
+          <FaBars size={32} />
+        </button>
+        <button className="cursor-pointer active:scale-95 transition-transform duration-100 ease-in-out flex items-center gap-2">
+          <FaRightToBracket size={32} />
+          <span className="text-xl font-medium hidden md:block">Sign in</span>
+        </button>
+      </div>
+      <div
+        className={`fixed inset-0 bg-white flex flex-col ${
+          showMenu ? "" : "translate-x-full"
+        } transition-transform duration-400 ease-in-out`}
+      >
+        <button
+          onClick={() => setShowMenu(false)}
+          className="flex items-center gap-2 text-xl cursor-pointer font-medium p-4 border-b-2 border-gray-200"
+        >
+          <FaArrowLeft size={24} />
+          Back
+        </button>
+        <Link
+          href="/"
+          className="text-xl font-medium  p-4 border-b-2 border-gray-200 "
+        >
+          Home
+        </Link>
+        <Link
+          href="/about-us"
+          className="text-xl font-medium  p-4 border-b-2 border-gray-200"
+        >
+          About us
+        </Link>
+        <Link
+          href="get-started"
+          className="text-xl font-medium  p-4 border-b-2 border-gray-200"
+        >
+          Get started
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
